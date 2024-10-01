62f524e99b3246d5228db04a1f2e892798d91e08
Alvine TEUBO <alvineteubo@gmail.com>
2024-10-01 15:03:29 +0100

    Implement app navigation [Group 1]

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import {
 } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/login/Login";
+import { AppNavigation } from "./components/appNavigation/AppNavigation.tsx";
+import { Contacts } from "./pages/contacts/Contacts.tsx";
+import { Medications } from "./pages/medications/Medications.tsx";
+import { Therapies } from "./pages/therapies/Therapies.tsx";
+import { Layout } from "./components/layout/Layout.tsx";
 
 const router = createBrowserRouter([
   {
@@ -12,13 +17,26 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
-    path: "/dashboard",
-    element: <Dashboard />,
+    path: "/",
+    loader: () => redirect("/login"),
   },
 
   {
-    path: "/",
-    loader: () => redirect("/login"),
+    element: <Layout />,
+    children: [
+      {
+        path: "/medications",
+        element: <Medications />,
+      },
+      {
+        path: "/contacts",
+        element: <Contacts />,
+      },
+      {
+        path: "/Therapies",
+        element: <Therapies />,
+      },
+    ],
   },
 ]);
 
--- src/components/appNavigation/AppNavigation.tsx
+++ src/components/appNavigation/AppNavigation.tsx
@@ -1,14 +1,78 @@
+import React, { useEffect } from "react";
+import "./AppNavigation.css";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { IconButton, Link, Typography } from "@mui/material";
+import contacts from "../../assets/images/appNavigation/contacts.png";
+import contactsRed from "../../assets/images/appNavigation/contactsRed.png";
+import homeHealth from "../../assets/images/appNavigation/home_health.png";
+import homeHealthRed from "../../assets/images/appNavigation/home_healthred.png";
+import prescription from "../../assets/images/appNavigation/prescriptions.png";
+import prescriptionRed from "../../assets/images/appNavigation/prescriptionsRed.png";
 
+const navItems = [
+  {
+    path: "/medications",
+    name: "Medications",
+    icon: homeHealth,
+    activeIcon: homeHealthRed,
+  },
+  {
+    path: "/therapies",
+    name: "Therapies",
+    icon: prescription,
+    activeIcon: prescriptionRed,
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    icon: contacts,
+    activeIcon: contactsRed,
+  },
+];
 
-export function AppNavigation (){
+export function AppNavigation() {
+  const location = useLocation();
 
+  if (location.pathname === "/login") {
+    return null;
+  }
 
+  const [activepage, setActivePage] = useState(location.pathname);
 
+  useEffect(() => {
+    setActivePage(location.pathname);
+  }, [location.pathname]);
 
-    return(
-        <div>
-
-
-        </div>
-    )
+  return (
+    <div className="mytherapie">
+      <div className="menuBot">
+        {navItems.map(({ path, name, icon, activeIcon }) => (
+          <div key={path} className="menuBottomItem">
+            <div
+              className={activepage === path ? "iconClickMiddle" : "iconClick"}
+            >
+              <IconButton
+                component={Link}
+                href={path}
+                onClick={() => setActivePage(path)}
+              >
+                <img
+                  src={activepage === path ? activeIcon : icon}
+                  alt="Home Health"
+                />
+              </IconButton>
+            </div>
+            <Typography
+              className={
+                activepage === path ? "menuBotIconName-over" : "menuBotIconName"
+              }
+            >
+              {name}
+            </Typography>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
