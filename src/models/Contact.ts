5c90bdba6e63051b6117148a03143968342b3f7a
Lidelle123 <108597170+Lidelle123@users.noreply.github.com>
2024-10-01 16:24:53 +0100

    gestion-des-erreures

---
+++ src/models/Contact.ts
@@ -0,0 +1,11 @@
+
+export interface IContacts {
+    id: number;
+    name: string;
+    notes: string;
+    qualification: string;
+    profession: string;
+    phone: string;
+    email: string;
+    address: string;
+  }
--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -6,26 +6,21 @@ import search from "../../assets/images/contact/Icon.svg";
 import add from "../../assets/images/contact/add_circle.svg";
 import arrowBack from "../../assets/images/contact/arrow_forward_ios.svg";
 import stethoscope from "../../assets/images/contact/stethoscope.svg";
-
-interface IContacts {
-  id: number;
-  name: string;
-  notes: string;
-  qualification: string;
-  profession: string;
-  phone: string;
-  email: string;
-  address: string;
-}
+import { IContacts } from "../../models/Contact";
 
 function Contacts() {
   const [contacts, setContacts] = useState<IContacts[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   const getContacts = async (): Promise<void> => {
-    const result = await fetch("http://localhost:3000/contacts");
-    const datas: IContacts[] = await result.json();
-    setContacts(datas);
+    try {
+      const result = await fetch("http://localhost:3000/contacts");
+      const datas: IContacts[] = await result.json();
+      setContacts(datas);
+    } catch (error) {
+      setError("failed to load contacts");
+    }
   };
 
   useEffect(() => {
@@ -34,104 +29,113 @@ function Contacts() {
 
   const filteredContacts = contacts?.filter(
     (contact) =>
-      contact.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      contact.profession.toLowerCase().includes(searchQuery.toLowerCase())
+      contact.name.includes(searchQuery) ||
+      contact.profession.includes(searchQuery)
   );
 
   function emptyContacts(contacts: IContacts[]): boolean {
     return contacts.length === 0;
   }
+  function isNull(error: string): boolean {
+    return error === "";
+  }
+
+  console.log(error);
 
   return (
-    <div className="container">
-      <div>
-        <Typography className="typography">Contacts</Typography>
-        <div className="searchContainer">
-          <Paper
-            component="div"
-            sx={{
-              p: "2px 4px",
-              display: "flex",
-              alignItems: "center",
-              width: "90%",
-              borderRadius: 20,
-              backgroundColor: "#FFEFEF",
-              maxHeight: 300,
-              overflowY: "auto",
-            }}
-          >
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              placeholder="Search doctor"
-              inputProps={{ "aria-label": "search doctor" }}
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-              <img src={search} alt="search icon" />
-            </IconButton>
-          </Paper>
+    <>
+      <div className="container">
+        <div>
+          <Typography className="typography">Contacts</Typography>
+          <div className="searchContainer">
+            <Paper
+              component="div"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: "90%",
+                borderRadius: 20,
+                backgroundColor: "#FFEFEF",
+                maxHeight: 300,
+                overflowY: "auto",
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search doctor"
+                inputProps={{ "aria-label": "search doctor" }}
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+                <img src={search} alt="search icon" />
+              </IconButton>
+            </Paper>
 
-          <div className="listContact">
-            {emptyContacts(contacts) ? (
-              <Typography>No contact available</Typography>
-            ) : (
-              filteredContacts.map((contact) => (
-                <Paper
-                  key={contact.id}
-                  component="form"
-                  sx={{
-                    p: "2px 4px",
-                    display: "flex",
-                    width: "90%",
-                    justifyContent: "space-between",
-                    backgroundColor: "#F4F4F4",
-                    paddingTop: 1.5,
-                    paddingBottom: 1.5,
-                  }}
-                >
-                  <IconButton
-                    type="button"
-                    sx={{ p: "10px" }}
-                    aria-label="stethoscope"
+            <div className="listContact">
+              {!isNull(error) ? (
+                <Typography sx={{ color: "red" }}>{error}</Typography>
+              ) : emptyContacts(contacts) ? (
+                <Typography>No contact available</Typography>
+              ) : (
+                filteredContacts.map((contact: IContacts) => (
+                  <Paper
+                    key={contact.id}
+                    component="form"
+                    sx={{
+                      p: "2px 4px",
+                      display: "flex",
+                      width: "90%",
+                      justifyContent: "space-between",
+                      backgroundColor: "#F4F4F4",
+                      paddingTop: 1.5,
+                      paddingBottom: 1.5,
+                    }}
                   >
-                    <img src={stethoscope} alt="stethoscope icon" />
-                  </IconButton>
-
-                  <div className="contactName">
-                    <Typography
-                      sx={{ fontSize: 17, fontWeight: 700 }}
-                      className="typography1"
+                    <IconButton
+                      type="button"
+                      sx={{ p: "10px" }}
+                      aria-label="stethoscope"
                     >
-                      {contact.qualification}. {contact.name}
-                    </Typography>
+                      <img src={stethoscope} alt="stethoscope icon" />
+                    </IconButton>
 
-                    <Typography
-                      sx={{ fontSize: 10, fontWeight: 400 }}
-                      className="typography2"
+                    <div className="contactName">
+                      <Typography
+                        sx={{ fontSize: 17, fontWeight: 700 }}
+                        className="typography1"
+                      >
+                        {contact.qualification}. {contact.name}
+                      </Typography>
+
+                      <Typography
+                        sx={{ fontSize: 10, fontWeight: 400 }}
+                        className="typography2"
+                      >
+                        {contact.profession}
+                      </Typography>
+                    </div>
+
+                    <IconButton
+                      type="button"
+                      sx={{ p: "10px" }}
+                      aria-label="arrowBack"
                     >
-                      {contact.profession}
-                    </Typography>
-                  </div>
-
-                  <IconButton
-                    type="button"
-                    sx={{ p: "10px" }}
-                    aria-label="arrowBack"
-                  >
-                    <img src={arrowBack} alt="arrowBack icon" />
-                  </IconButton>
-                </Paper>
-              ))
-            )}
+                      <img src={arrowBack} alt="arrowBack icon" />
+                    </IconButton>
+                  </Paper>
+                ))
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="addContainer">
-        <img src={add} alt="add icon" />
+        <div className="addContainer">
+          <img src={add} alt="add icon" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
