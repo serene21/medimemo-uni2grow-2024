31f8aac7fe17cdc3cf8ae4514f8aef77e8a050da
BILL <tatemsabill@gmail.com>
2024-10-09 10:58:27 +0200

    Gestion du fonctionnement de tous les elements de l'ajout

--- src/pages/addEditTherapie/AddEditTherapie.tsx
+++ src/pages/addEditTherapie/AddEditTherapie.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, useEffect, FormEvent } from "react";
 import {
   TextField,
   IconButton,
@@ -9,8 +9,8 @@ import {
   InputLabel,
   FormControl,
   Typography,
+  SelectChangeEvent,
 } from "@mui/material";
-import WestIcon from "@mui/icons-material/West";
 import SaveIcon from "@mui/icons-material/Save";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useNavigate } from "react-router-dom";
@@ -18,50 +18,143 @@ import "./AddEditTherapie.css";
 import {
   formError,
   validationTherapy,
-  ITherapy
+  ITherapy,
+  therapyForm,
+  formValues,
+  isNoEmpty,
 } from "../../utils/Validation";
 import Header from "../../components/header/Header";
+import { IMedecine } from "../../models/Medecine";
+import { IContact } from "../../models/Contact";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
+import InputAdornment from "@mui/material/InputAdornment";
 
 function AddEditTherapie() {
-  const [therapy, setTherapy] = useState<ITherapy>({
+  const [therapies, setTherapies] = useState<formValues>({
     name: "",
   });
-  const [medication, setMedication] = useState("");
+  const [medicationError, setMedicationError] = useState<string>("");
+  const [medicineSelected, setMedicineSelected] = useState<string[]>([]);
+  const [medicines, setMedicines] = useState<IMedecine[]>([]);
+  const [contacts, setContacts] = useState<IContact[]>([]);
+  const [doctorError, setDoctorError] = useState<string>("");
+  const [doctors, setDoctors] = useState<string>("");
+  const [addError, setAddError] = useState<string>("");
+
   const navigate = useNavigate();
 
   const [errors, setErrors] = useState<formError>({
     name: "",
   });
 
-  const handleSave = () => {
-    console.log("Therapy saved:", therapy, medication);
-    // Logique pour enregistrer la thérapie
+  const getMedication = async (): Promise<void> => {
+    try {
+      setMedicationError("");
+      const response = await fetch("http://localhost:80/medicines");
+      const data = await response.json();
+      setMedicines(data);
+    } catch {
+      setMedicationError("Failed to load medicines");
+    }
   };
 
+  const getDoctor = async (): Promise<void> => {
+    try {
+      const response = await fetch("http://localhost:80/contacts");
+      const data = await response.json();
+      setContacts(data);
+    } catch {
+      setDoctorError("Failed to load medecines");
+    }
+  };
+
+  useEffect(() => {
+    getMedication();
+    getDoctor();
+  }, []);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     const error = validationTherapy(name, value);
-    
     setErrors((prevState) => ({
-        ...prevState,
-        [name]: error || "",
+      ...prevState,
+      [name]: error || "",
     }));
 
-    setTherapy((prevState) => ({
-        ...prevState,
-        [name] : value,
+    setTherapies((prevState) => ({
+      ...prevState,
+      [name]: value,
     }));
-  }
+  };
+
+  const handleSelect = (e:SelectChangeEvent<string>): void =>{
+    const existMedicine = medicineSelected.filter((item)=> item === e.target.value);
+    if(existMedicine.length === 0){
+      setMedicineSelected([...medicineSelected, e.target.value])
+    }
+  };
 
   const handleBack = () => {
-    navigate(-1); // Retourne à la page précédente
+    navigate(-1);
+  };
+
+  const handleDoctor = (e: SelectChangeEvent<string>): void => {
+    setDoctors(e.target.value);
+    setErrors((prevState) => ({
+      ...prevState,
+      doctor: "",
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    const validationErrors = therapyForm(therapies);
+  
+    if (Object.keys(validationErrors).length === 0) {
+      
+      if (medicineSelected.length !== 0 && doctors) {
+        try{
+          const newTherapy:ITherapy = {
+            id:2,
+            name: therapies.name,
+            userId: 1,
+            contact: 2,
+            notes : "this is the notes"
+          };
+          const result = await fetch("http://localhost:80/therapies",{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+  
+            body: JSON.stringify(newTherapy),
+          });
+          if(!result.ok){
+            setAddError("Add therapy failed");
+          }
+          handleBack();
+        } catch {
+          setAddError("Add  therapy failed");
+        } 
+        
+      } else {
+        console.log("l'erreur");
+        setErrors((prevState) => ({
+          ...prevState,
+          doctor: "This field is required",
+        }));
+      }
+    } else {
+      setErrors(validationErrors);
+    }
   };
 
   return (
     <>
-      <div className="therapy-page">
-        <Header title="" showBackButton onBackButtonClick={handleBack}/>
-    
+      <form className="therapy-page" onSubmit={handleSubmit}>
+        <Header title="" showBackButton onBackButtonClick={handleBack} />
+
         <div className="therapyContent">
           <Box className="element" sx={{ gap: "20" }}>
             <Box
@@ -80,96 +173,204 @@ function AddEditTherapie() {
               onChange={handleChange}
               error={!!errors.name}
               helperText={errors.name}
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {medicineSelected.length !== 0 ? (
+                        <HighlightOffIcon sx={{ color: "#F00" }} />
+                      ) : null}
+                    </InputAdornment>
+                  ),
+                },
+              }}
             />
           </Box>
-          <Box className="element" sx={{ gap: "20" }}>
-            <Box
-              sx={{
-                width: "100%",
-              }}
-            >
-              Select one ore more therapy
-            </Box>
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Select one or more medicines</InputLabel>
-              <Select
-                value={medication}
-                onChange={(e) => setMedication(e.target.value)}
-                label="Select one or more medicines"
-              >
-                <MenuItem value="med1">Medicine 1</MenuItem>
-                <MenuItem value="med2">Medicine 2</MenuItem>
-                <MenuItem value="med3">Medicine 3</MenuItem>
-              </Select>
-            </FormControl>
-            <div className="panel-element">
-              <div className="medecine-name">
-                <div className="medecine-name-title">
-                  <Box className="medecine-title">Drop SEPT</Box>
-                </div>
-                <div className="medecine-title-button">
-                  <ArrowForwardIosIcon />
-                </div>
-              </div>
-              <div className="aline-button">
-                <div className="medecine-name-button">
-                  <Typography className="medecine-button">REMOVE</Typography>
-                </div>
-                <div className="medecine-name-button">
-                  <Typography className="medecine-button">
-                    ADD PROGRAM
-                  </Typography>
-                </div>
-              </div>
-            </div>
-          </Box>
+          {medicineSelected.length !== 0 ? (
+            <>
+              {medicineSelected.map((item, index) => (
+                <Box className="element" key={index} sx={{ gap: "20" }}>
+                  <div className="panel-element">
+                    <div className="medecine-name">
+                      <div className="medecine-name-title">
+                        <Box className="medecine-title">{item}</Box>
+                      </div>
+                      <div className="medecine-title-button">
+                        <ArrowForwardIosIcon />
+                      </div>
+                    </div>
+                    <div className="aline-button">
+                      <div
+                        onClick={() =>
+                          setMedicineSelected(
+                            medicineSelected.filter((value) => value !== item)
+                          )
+                        }
+                        className="medecine-name-button"
+                      >
+                        <Button
+                          sx={{ color: "white" }}
+                          className="medecine-button"
+                        >
+                          REMOVE
+                        </Button>
+                      </div>
+                      <div className="medecine-name-button">
+                        <Button
+                          sx={{ color: "white" }}
+                          className="medecine-button"
+                        >
+                          ADD PROGRAM
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </Box>
+              ))}
 
-          <Box className="element" sx={{ gap: "20" }}>
-            <Box
-              sx={{
-                width: "100%",
-              }}
-            >
-              Select a doctor
+              <Box className="element" sx={{ gap: "20" }}>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  Add another medication
+                </Box>
+
+                <FormControl fullWidth margin="normal">
+                  <InputLabel>Search here your medecine</InputLabel>
+                  <Select
+                    value=""
+                    onChange={handleSelect
+                    }
+                    label="Select one or more medicines"
+                  >
+                    {!medicationError ? (
+                      medicines.map((item) => {
+                        return (
+                          <MenuItem key={item.id} value={item.name}>
+                            {item.name}
+                          </MenuItem>
+                        );
+                      })
+                    ) : (
+                      <MenuItem>{medicationError}</MenuItem>
+                    )}
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box className="element" sx={{ gap: "20" }}>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  Select a doctor
+                </Box>
+
+                <FormControl fullWidth margin="normal">
+                  <InputLabel>Select a doctor</InputLabel>
+                  <Select
+                    value={doctors}
+                    name="doctor"
+                    label="Select a doctor"
+                    onChange={handleDoctor}
+                    error={!!errors.doctor}
+                  >
+                    {contacts.length !== 0 ? (
+                      contacts.map((item) => {
+                        return (
+                          <MenuItem key={item.id} value={item.name}>
+                            {item.name}
+                          </MenuItem>
+                        );
+                      })
+                    ) : (
+                      <MenuItem>{doctorError}</MenuItem>
+                    )}
+                  </Select>
+                  {errors.doctor && (
+                    <p className="decoration">{errors.doctor}</p>
+                  )}
+                </FormControl>
+              </Box>
+              <Box className="element" sx={{ gap: "20" }}>
+                <Box
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  Notes
+                </Box>
+                <TextField
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={3}
+                  className="therapieField"
+                  sx={{ width: "100%", height: "83px" }}
+                  margin="normal"
+                  label="Write your notes here"
+                />
+              </Box>
+            </>
+          ) : (
+            <Box className="element" sx={{ gap: "20" }}>
+              <Box
+                sx={{
+                  width: "100%",
+                }}
+              >
+                Select one ore more therapy
+              </Box>
+
+              <FormControl fullWidth margin="normal">
+                <InputLabel>Select one or more medicines</InputLabel>
+                <Select
+                  value=""
+                  onChange={(e) =>
+                    setMedicineSelected([...medicineSelected, e.target.value])
+                  }
+                  label="Select one or more medicines"
+                >
+                  {medicines.map((item) => {
+                    return (
+                      <MenuItem key={item.id} value={item.name}>
+                        {item.name}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
             </Box>
-            <TextField
-              label="Select a doctor"
-              className="therapieField"
-              sx={{ width: "100%" }}
-              margin="normal"
-            />
-          </Box>
-          <Box className="element" sx={{ gap: "20" }}>
-            <Box
-              sx={{
-                width: "100%",
-              }}
-            >
-              Notes
-            </Box>
-            <TextField
-              id="outlined-multiline-static"
-              multiline
-              rows={3}
-              className="therapieField"
-              sx={{ width: "100%", height: "83px" }}
-              margin="normal"
-            />
-          </Box>
+          )}
         </div>
         <div className="bottom-page">
-          <Button
-            className="bottom-button"
-            sx={{
-              backgroundColor: "#F00",
-              color: "white",
-              width: "100%",
-            }}
-          >
-            <SaveIcon /> Save
-          </Button>
+          {medicineSelected.length === 0 ? (
+            <Button
+              className="bottom-button"
+              sx={{
+                backgroundColor: "#F00",
+                color: "white",
+                width: "100%",
+              }}
+            >
+              <SaveIcon /> Save
+            </Button>
+          ) : (
+            <Button
+              className="bottom-button-active"
+              sx={{
+                backgroundColor: "#F00",
+                color: "white",
+                width: "100%",
+              }}
+              type="submit"
+            >
+              <SaveIcon /> Save
+            </Button>
+          )}
         </div>
-      </div>
+      </form>
     </>
   );
 }
