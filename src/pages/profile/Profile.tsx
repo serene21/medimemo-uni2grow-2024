418de8cc0ad019f67d623d70591d7ae6c516893b
duvaldev203 <74819278+duvaldev203@users.noreply.github.com>
2024-10-09 10:20:39 +0100

    Static profile user doned

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,5 +1,17 @@
 import "./Profile.css";
-import { Avatar, Button, InputAdornment, TextField, Typography, IconButton } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  InputAdornment,
+  TextField,
+  Typography,
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
 import UserAvatar from "../../assets/images/avatar.svg";
 import UniversalCur from "../../assets/images/profile/universal_currency.svg";
 import Allergies from "../../assets/images/profile/allergies.svg";
@@ -10,7 +22,14 @@ import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import Header from "../../components/header/Header";
 import { useNavigate } from "react-router-dom";
 import { FormEvent, useState } from "react";
-import { Cancel, Edit, LocalSee } from "@mui/icons-material";
+import {
+  ArrowBackIos,
+  Cancel,
+  Edit,
+  LocalSee,
+  Logout,
+  ReportOutlined,
+} from "@mui/icons-material";
 
 const data = {
   name: "Francesca Greco",
@@ -25,23 +44,32 @@ const Profile = () => {
   const navigate = useNavigate();
   const [formValues, setFormValues] = useState(data);
   const [editable, setEditable] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
 
   const handleEdit = () => {
     setEditable(!editable);
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    if (editable) handleLogout(e);
-    else handleSave(e);
+    e.preventDefault();
+    if (!editable) handleOpenAlert();
+    else handleSave();
     setEditable(false);
   };
 
-  const handleSave = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSave = () => {
     console.log("You data has been saved !!!");
   };
-  const handleLogout = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+
+  const handleOpenAlert = () => {
+    setOpenAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+
+  const handleLogout = () => {
     navigate("/login");
   };
 
@@ -52,8 +80,8 @@ const Profile = () => {
 
   const handleClean = (name: string) => {
     // const { name } = e.name;
-    setFormValues((prevForm) => ({ ...prevForm, [name]: "" }))
-  }
+    setFormValues((prevForm) => ({ ...prevForm, [name]: "" }));
+  };
 
   return (
     <div className="first-container">
@@ -62,32 +90,94 @@ const Profile = () => {
         showBackButton={true}
         showRightButton={!editable && true}
         RightButton={<Edit />}
-        onBackButtonClick={() => navigate("/medications")}
+        // onBackButtonClick={() => navigate("/medications")}
+        onBackButtonClick={() => navigate(-1)}
         onRightButtonClick={handleEdit}
       />
+      <Dialog
+        open={openAlert}
+        onClose={handleCloseAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{
+          color: "#F00",
+        }}
+      >
+        <DialogTitle
+          variant="h6"
+          id="alert-dialog-title"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#222",
+            gap: "10px",
+            fontWeight: "bold",
+          }}
+        >
+          <ReportOutlined sx={{ color: "#555" }} />
+          Log-out Confirmation
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to log out? Any unsaved changes will be lost.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleCloseAlert}
+            sx={{
+              textTransform: "capitalize",
+              color: "#555",
+              fontWeight: "bold",
+            }}
+          >
+            <ArrowBackIos />
+            &nbsp;Back
+          </Button>
+          <Button
+            sx={{
+              textTransform: "capitalize",
+              color: "#F00",
+              fontWeight: "bold",
+            }}
+            onClick={handleLogout}
+            autoFocus
+          >
+            <Logout />
+            Log-out
+          </Button>
+        </DialogActions>
+      </Dialog>
       <form className="profile-container" onSubmit={handleSubmit}>
         <div className="secondary">
           <div className="avatar-container">
-            <Avatar src={UserAvatar} alt="Avatar" sx={{ width: "100px", height: "auto" }} />
-            {editable && <IconButton
-              aria-label="profile"
-              onClick={() => console.log("cliked")}
-              sx={{
-                position: "absolute",
-                bottom: "0px",
-                right: "0px",
-                padding: "5px",
-                backgroundColor: "#F00",
-                color: "#FFF",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: "50%"
-              }}
-            >
-              <LocalSee sx={{ fontSize: "1rem" }} />
-
-            </IconButton>}
+            <Avatar
+              src={UserAvatar}
+              alt="Avatar"
+              sx={{ width: "100px", height: "auto" }}
+            />
+            {editable && (
+              <IconButton
+                aria-label="profile"
+                onClick={() => console.log("cliked")}
+                sx={{
+                  position: "absolute",
+                  bottom: "0px",
+                  right: "0px",
+                  padding: "5px",
+                  backgroundColor: "#F00",
+                  color: "#FFF",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "50%",
+                }}
+              >
+                <LocalSee sx={{ fontSize: "1rem" }} />
+              </IconButton>
+            )}
           </div>
           <div className="credentials">
             <Typography variant="h6" color="initial">
@@ -114,17 +204,20 @@ const Profile = () => {
                     ),
                     endAdornment: (
                       <>
-                        {editable &&
+                        {editable && (
                           <>
-                            {formValues.medicalID != "" &&
-                              <IconButton sx={{ color: "#F00", p: "0px" }} onClick={() => handleClean("medicalID")} >
+                            {formValues.medicalID != "" && (
+                              <IconButton
+                                sx={{ color: "#F00", p: "0px" }}
+                                onClick={() => handleClean("medicalID")}
+                              >
                                 <Cancel />
                               </IconButton>
-                            }
+                            )}
                           </>
-                        }
+                        )}
                       </>
-                    )
+                    ),
                   },
                 }}
               />
@@ -148,17 +241,20 @@ const Profile = () => {
                     ),
                     endAdornment: (
                       <>
-                        {editable &&
+                        {editable && (
                           <>
-                            {formValues.allergies != "" &&
-                              <IconButton sx={{ color: "#F00", p: "0px" }} onClick={() => handleClean("allergies")} >
+                            {formValues.allergies != "" && (
+                              <IconButton
+                                sx={{ color: "#F00", p: "0px" }}
+                                onClick={() => handleClean("allergies")}
+                              >
                                 <Cancel />
                               </IconButton>
-                            }
+                            )}
                           </>
-                        }
+                        )}
                       </>
-                    )
+                    ),
                   },
                 }}
               />
@@ -178,17 +274,20 @@ const Profile = () => {
                     ),
                     endAdornment: (
                       <>
-                        {editable &&
+                        {editable && (
                           <>
-                            {formValues.phone != "" &&
-                              <IconButton sx={{ color: "#F00", p: "0px" }} onClick={() => handleClean("phone")} >
+                            {formValues.phone != "" && (
+                              <IconButton
+                                sx={{ color: "#F00", p: "0px" }}
+                                onClick={() => handleClean("phone")}
+                              >
                                 <Cancel />
                               </IconButton>
-                            }
+                            )}
                           </>
-                        }
+                        )}
                       </>
-                    )
+                    ),
                   },
                 }}
               />
@@ -209,17 +308,20 @@ const Profile = () => {
                     ),
                     endAdornment: (
                       <>
-                        {editable &&
+                        {editable && (
                           <>
-                            {formValues.email != "" &&
-                              <IconButton sx={{ color: "#F00", p: "0px" }} onClick={() => handleClean("email")} >
+                            {formValues.email != "" && (
+                              <IconButton
+                                sx={{ color: "#F00", p: "0px" }}
+                                onClick={() => handleClean("email")}
+                              >
                                 <Cancel />
                               </IconButton>
-                            }
+                            )}
                           </>
-                        }
+                        )}
                       </>
-                    )
+                    ),
                   },
                 }}
               />
@@ -243,15 +345,20 @@ const Profile = () => {
                     ),
                     endAdornment: (
                       <>
-                        {editable &&
-                          <>{formValues.address != "" &&
-                            <IconButton sx={{ color: "#F00", p: "0px" }} onClick={() => handleClean("address")} >
-                              <Cancel />
-                            </IconButton>}
+                        {editable && (
+                          <>
+                            {formValues.address != "" && (
+                              <IconButton
+                                sx={{ color: "#F00", p: "0px" }}
+                                onClick={() => handleClean("address")}
+                              >
+                                <Cancel />
+                              </IconButton>
+                            )}
                           </>
-                        }
+                        )}
                       </>
-                    )
+                    ),
                   },
                 }}
               />
@@ -282,8 +389,8 @@ const Profile = () => {
             </>
           )}
         </Button>
-      </form >
-    </div >
+      </form>
+    </div>
   );
 };
 
